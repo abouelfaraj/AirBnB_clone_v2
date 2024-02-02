bc901cb13c826d6d8d0cf79bcd2a92006f396de4
root <aef.ayoub@gmail.com>
2024-02-02 07:09:58 -0800

    cmt task 3 - coorect filename output

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,8 +3,7 @@
 
 from datetime import datetime
 from fabric.api import *
-import shlex
-import os
+from os.path import exists, isdir
 
 
 env.hosts = ['34.207.155.207', '18.206.208.206']
@@ -13,11 +12,11 @@ env.hosts = ['34.207.155.207', '18.206.208.206']
 def do_pack():
     """Creates archive tgz"""
     try:
-        date = datetime.now().strftime("")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
-            local("mkdir versions")
+            local("sudo mkdir versions")
         filename = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(filename))
+        local("sudo tar -cvzf {} web_static".format(filename))
         return filename
     except:
         return None
@@ -25,7 +24,7 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Fabric script that distributes an archive to server web-01 and web-02"""
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
     try:
         name = archive_path.split("/")[-1]
@@ -33,13 +32,13 @@ def do_deploy(archive_path):
         path = "/data/web_static/releases/"
         
         put(archive_path, "/tmp/")
-        run("mkdir -p {}{}/".format(path, name_inex))
-        run("tar -xzf /tmp/{} -C {}{}/".format(name, path, name_inex))
-        run("rm /tmp/{}".format(name))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, name_inex))
-        run("rm -rf {}{}/web_static".format(path, name_inex))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, name_inex))
+        run("sudo mkdir -p {}{}/".format(path, name_inex))
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(name, path, name_inex))
+        run("sudo rm /tmp/{}".format(name))
+        run("sudo mv {0}{1}/web_static/* {0}{1}/".format(path, name_inex))
+        run("sudo rm -rf {}{}/web_static".format(path, name_inex))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(path, name_inex))
         return True
     except:
         return False
